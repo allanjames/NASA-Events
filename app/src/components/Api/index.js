e45dccbfe52bd932b986f77aa7ff15a9f7fb9fa7
allanjames <james.allan@brafton.com>
2019-01-02 10:19:58 -0500

    delivering api items to screen

--- app/src/components/Api/index.js
+++ app/src/components/Api/index.js
@@ -6,19 +6,29 @@ class Api extends Component {
     message = 'Inside Api component on ';
     time = new Date();
     
-    stat = {
+    state = {
         obj: []
     };
     componentDidMount(){
         
-        fetch('https://api.nasa.gov/planetary/apod?api_key='+process.env.REACT_APP_API_KEY)
+        fetch('https://api.nasa.gov/DONKI/CME?startDate=2017-01-01&endDate=2017-01-31&api_key='+process.env.REACT_APP_API_KEY)
         .then(response => response.json())
-         .then(json => { console.log(json) })
+         .then(json => { this.setState({obj: json}); console.log(this.state.obj)  })
          .catch(error => console.log(error))
     }
 
     render() {
-      return (<div>{this.message}{this.time.toUTCString()}</div>);
+      return (
+            <div><p>{this.message}{this.time.toUTCString()}</p>
+            { <ul className="nasa-list">
+                {this.state.obj.map( item => (
+                    <li key={item.activityID}>{item.note}</li>
+                    )
+                )}
+            </ul> }
+            </div>
+
+        );
     }
 }
 export default Api;
